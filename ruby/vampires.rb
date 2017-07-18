18570fb89f94dc34f12f1a813083d7b37e32232e
Kadin <kadinvanvalin@yahoo.com>
2017-07-18 07:02:59 -0400

    add bool_converter function

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,33 @@
 puts "What is your name?"
-ans1 = gets.chomp
+employee_name = gets.chomp
 puts "How old are you?" 
-ans2 = gets.chomp
+age = gets.chomp
 puts "What year were you born?"
-ans3 = gets.chomp
-puts "Our company cafereia serves Galic bread. should we order some for you?"
-ans4 = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
-ans5 = gets.chomp
+dob = gets.chomp
+puts "Our company cafereia serves Galic bread. should we order some for you? Please enter yes or no"
+like_garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance? Please enter yes or no"
+insurance = gets.chomp
+
+
+
+
+
+def bool_converter(string) 
+		if string == "yes"
+			return true
+		elsif string == "no"
+			return  false
+		end
+end	
+
+like_garlic_bool = bool_converter(like_garlic)
+insurance_bool = bool_converter(insurance)	
+
+puts like_garlic_bool
+puts insurance_bool
+
+
+# if age.to_i < 100 && like_garlic || insurance
+# 	puts "test"
+# end
