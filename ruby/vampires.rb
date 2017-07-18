2db3b7ddd92445b50052ae3e63b0dfb486526084
Kadin <kadinvanvalin@yahoo.com>
2017-07-18 06:39:38 -0400

    add release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+ans1 = gets.chomp
+puts "How old are you?" 
+ans2 = gets.chomp
+puts "What year were you born?"
+ans3 = gets.chomp
+puts "Our company cafereia serves Galic bread. should we order some for you?"
+ans4 = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+ans5 = gets.chomp
