1de92624de11acf40856461e270ea1993cf6fa47
Kadin <kadinvanvalin@yahoo.com>
2017-07-19 14:43:40 -0400

    add release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,33 +1,53 @@
-puts "What is your name?"
-employee_name = gets.chomp
-puts "How old are you?" 
-age = gets.chomp
-puts "What year were you born?"
-dob = gets.chomp
-puts "Our company cafereia serves Galic bread. should we order some for you? Please enter yes or no"
-like_garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance? Please enter yes or no"
-insurance = gets.chomp
 
 
 
 
+
+def process_Multiple int
+  i = 0
+  until i == int
+  puts "What is your name?"
+  employee_name = gets.chomp
+  puts "How old are you?" 
+  age = gets.chomp
+  puts "What year were you born?"
+  dob = gets.chomp
+  puts "Our company cafereia serves Garlic bread. should we order some for you? Please enter yes or no"
+  like_garlic = gets.chomp
+  puts "Would you like to enroll in the company's health insurance? Please enter yes or no"
+  insurance = gets.chomp
+
+  
 
 def bool_converter(string) 
-		if string == "yes"
-			return true
-		elsif string == "no"
-			return  false
-		end
-end	
+    if string == "yes"
+      return true
+    elsif string == "no"
+      return  false
+    end
+end 
 
 like_garlic_bool = bool_converter(like_garlic)
-insurance_bool = bool_converter(insurance)	
+insurance_bool = bool_converter(insurance)  
+  if employee_name == "Drake Cula"
+    puts "#{employee_name} is Definitely a Vampire"
+    elsif age.to_i > 100 && !like_garlic_bool && !insurance_bool
+      puts "#{employee_name} is almost certainly a Vampire"
+   elsif age.to_i > 100 && like_garlic_bool || insurance_bool
+     puts "#{employee_name} is probably a vampire"
+    elsif age.to_i < 100 && like_garlic_bool || insurance_bool
+    puts "#{employee_name} is probably not a Vampire"
+  else 
+     puts "Results inconclusive for #{employee_name}"
+  end
+end
+i += 1
+end
 
-puts like_garlic_bool
-puts insurance_bool
+puts "n
 
 
-# if age.to_i < 100 && like_garlic || insurance
-# 	puts "test"
-# end
+how many Employees are you processing?"
+number_of_employees = gets.chomp.to_i
+
+process_Multiple number_of_employees
