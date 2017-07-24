47efa6d80e6876b33e287ce0a1dc52ec612790dc
Kadin <kadinvanvalin@yahoo.com>
2017-07-24 18:24:23 -0400

    add conversion

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -11,7 +11,23 @@
 # 		ler the user update a key
 # print latest, exit		
 
-puts "What is the clients name?"
-puts "What is the clients age?"
-puts "How many children does the client have?"
-puts "How would you describe the clients style? please seperate each item by a \",\" "
+decor_theme = {
+	client_name: nil,
+	client_age: nil,
+	children_amount_of: nil,
+	clients_style: []
+
+}
+
+# puts "What is the clients name?"
+# client_name = gets.chomp
+# puts "What is the clients age?"
+# client_age = gets.chomp.to_i
+# puts "How many children does the client have?"
+# children_amount_of = gets.chomp.to_i
+ puts "How would you describe the clients style? please seperate each item by a \",\" "
+
+decor_theme[:clients_style] = gets.chomp.split(", ") 
+
+
+puts decor_theme
