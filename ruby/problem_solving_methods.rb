f1ad656cd4d1fc3e401f17d5a24b5619ceb4637d
Kadin <kadinvanvalin@yahoo.com>
2017-07-28 20:13:05 -0400

    add insertsort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -96,32 +96,78 @@
 # 	end
 
 # end
-def bubblesort ary, new_array
-	i = 0
-	if ary.length == 0
-		return new_array
-	else
-		ary.each do |number|
+# def bubblesort ary, new_array
+# 	i = 0
+# 	if ary.length == 0
+# 		return new_array
+# 	else
+# 		ary.each do |number|
 			
 			
-			puts i
-			next_number = ary[i+1]
-			if next_number == nil
-				p new_array
-				p ary
-				return bubblesort ary, new_array << ary.pop
+# 			puts i
+# 			next_number = ary[i+1]
+# 			if next_number == nil
+# 				p new_array
+# 				p ary
+# 				return bubblesort ary, new_array << ary.pop
 			
 
-			elsif number > ary[i+1]
-				ary[i+1] = number
-				ary[i] = next_number
+# 			elsif number < ary[i+1]
+# 				ary[i+1] = number
+# 				ary[i] = next_number
+# 			end
+# 			i+=1
+
+
+# 		end
+# 	end
+
+#end
+
+def insertsort(array)
+	new_array= []
+	while array.length > 0
+		if new_array.length == 0
+			new_array << array.shift
+		else
+			new_array.each_with_index.map do |element, index|
+				if array [0] > element && index = -1
+					new_array << array.shift
+				elsif array[0] > element
+					#do nothing
+				else 
+					new_array.insert(index, array.shift)
+				end
 			end
-			i+=1
-
-
 		end
 	end
-
+	return new_array
 end
-
  p bubblesort([0, 30, 50, 88, 3, 100, 20, 1 , 3,2], [])
+
+ def insertsort(array)
+	new_array= []
+	while array.length > 0
+		if new_array.length == 0
+			new_array << array.shift
+		else
+			new_array.each_with_index.map do |element, index|
+			  puts index
+			  p new_array
+			  p array
+			  if array == []
+			    return new_array
+				elsif array[0] >= element && index==new_array.length - 1
+				  p "pop"
+					new_array << array.shift
+				elsif array[0] < element && array[0] >= new_array[index-1]
+				 
+						new_array.insert(index, array.shift)
+				
+				end
+			end
+		end
+	end
+	return new_array
+end
+ p insertsort([0, 30, 50, 88, 3, 100, 20, 1 , 3,2])
