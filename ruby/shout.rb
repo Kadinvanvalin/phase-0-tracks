78cc4612124eb92d16515e9e177265eb1ffd8274
Kadin <kadinvanvalin@yahoo.com>
2017-08-05 11:07:39 -0400

    add release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,41 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
+
+# p Shout.yell_angrily("I am angrily yelling")
+
+# p Shout.yelling_happily("I am happily yelling")
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
+	def yelling_happily(words)
+			words + "!!!" + " :)"
 	end
 end
 
-p Shout.yell_angrily("I am angrily yelling")
+class Debugging_Session
+		include Shout
+end
 
-p Shout.yelling_happily("I am happily yelling")
+class Programming_Session
+	include Shout
+
+end
+
+kadin = Programming_Session.new
+
+
+p kadin.yelling_happily("I am coding")
+
+kadin = Debugging_Session.new
+
+p kadin.yell_angrily("Why doesn't it work?")
+
+p kadin.yelling_happily("I found the bug")
