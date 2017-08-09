a0c181663c64cd7d35792b43bb37d7c0434e92af
Kadin <kadinvanvalin@yahoo.com>
2017-08-09 17:08:50 -0400

    add pseudocode

---
+++ js/explore/explore.js
@@ -0,0 +1,9 @@
+module.exports.thisThing =  true
+
+
+// take a string into a function
+// turn the string into an array
+// 		repeat until array empty take the last char of the array and push it into a new array
+// 	convert array to string
+// 	compair new_string with the orignal sting, if it the same, return error
+// 	otherwise return new_string
